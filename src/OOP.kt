a0ce422a1c2b30d62de982c9d5a39f2bd26c1ba2
Iaramako <sugitbawls@gmail.com>
2020-02-07 18:35:14 +1300

    Constructors up to Class Boxer - Massie

--- src/OOP.kt
+++ src/OOP.kt
@@ -30,3 +30,24 @@ class Car{
 
 
 }
+//A Constructor of the Boxer class
+//inside the Boxer class are the variables
+
+class Boxer(name:String, power:Int, speed: Int) {
+    private var name: String = ""  // empty string value
+    private var power: Int? = null
+    private var speed: Int = 0
+
+
+    //init is executed the moment an object of a Boxer class is initialised.
+    init {
+        this.name =
+            name // this.refers to the instance variable or an object of the boxer , we get the name after the = sign from
+        // the para of constructor from the constructor
+    }
+    fun getName(): String {
+        return this.name
+
+    }
+
+}
