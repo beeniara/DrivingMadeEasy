bad645d112f064378ee92c7e1b471388ead81575
Iaramako <sugitbawls@gmail.com>
2020-02-10 10:01:58 +1300

    enum

---
+++ src/enum.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println(GameStatus.WON)
+    println(GameStatus.LOSS)
+    println(Feel.GOOD)
+    //is a data type consisting of set of named values called elements
+//    it contains integral constants
+// when variable can only one out of small set of possible values
+//    good or bad, young or old
+
+}
+
+
+    enum class GameStatus {
+        WON, LOSS
+    }
+    enum class Feel {
+        GOOD, BAD
+    }
